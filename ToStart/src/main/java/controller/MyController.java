dae74a41aa4621b02226941a0666266c10b00e5d
csabi1 <bodnarcsaba18@yahoo.hu>
2020-05-22 16:42:41 +0300

    listing done

--- ToStart/src/main/java/controller/MyController.java
+++ ToStart/src/main/java/controller/MyController.java
@@ -2,9 +2,12 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.returnVal;
+
+import java.util.ArrayList;
 
 
 public class MyController {
@@ -15,6 +18,9 @@ public class MyController {
     @FXML
     private Label myLabel;
 
+    @FXML
+    private ChoiceBox listOfMach;
+
     private returnVal val;
 
     @FXML
@@ -23,6 +29,17 @@ public class MyController {
     }
 
     public void iras(ActionEvent actionEvent) {
-       myLabel.setText(val.myReturn());
+
+       ArrayList<String> names = new ArrayList<>();
+
+      //  names = val.listOfNames();
+      //  int size = names.size();
+
+        for (int i =0 ; i < val.listOfNames().size()  ;i++) {
+            listOfMach.getItems().add(val.listOfNames().get(i));
+        }
+
+
+      //  myLabel.setText(val.myReturn());
     }
 }
